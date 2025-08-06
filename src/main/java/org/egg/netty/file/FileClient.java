a5dc23af0098af5f83e2069629702eb2033db5b5
adanli <adanli@126.com>
2025-08-06 19:43:09 +0800

    feat: day11, 为服务端添加HTTPS支持

--- src/main/java/org/egg/netty/file/FileClient.java
+++ src/main/java/org/egg/netty/file/FileClient.java
@@ -10,9 +10,14 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import org.egg.netty.file.handler.FileClientHandler;
 
+import javax.net.ssl.TrustManagerFactory;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
+import java.security.KeyStore;
 
 public class FileClient {
     private static final String SAVE_PATH = "/Users/adan/code/egg/netty-practice-30/src/main/resources/";
@@ -20,8 +25,9 @@ public class FileClient {
    private static final String HOST = "localhost";
     private static final int PORT = 8088;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         EventLoopGroup group = new NioEventLoopGroup();
+        SslContext sslContext = sslContext();
 
         try {
             Bootstrap bootstrap = new Bootstrap()
@@ -31,6 +37,8 @@ public class FileClient {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline()
+                                    .addLast(sslContext.newHandler(ch.alloc()))
+
                                     .addLast(new StringEncoder())
                                     .addLast(new FileClientHandler(SAVE_PATH))
                                     ;
@@ -55,6 +63,23 @@ public class FileClient {
             group.shutdownGracefully().sync();
         }
 
+
+    }
+
+    private static SslContext sslContext() throws Exception{
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+
+        try (InputStream inputStream = FileClient.class.getResourceAsStream("/client-truststore.p12")){
+            keyStore.load(inputStream, "clientpass".toCharArray());
+        }
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(keyStore);
+
+        return SslContextBuilder.forClient()
+                .trustManager(tmf)
+                .protocols("TLSv1.3", "TLSv1.2")
+                .build();
 
     }
 
--- src/main/java/org/egg/netty/file/FileServer.java
+++ src/main/java/org/egg/netty/file/FileServer.java
@@ -9,8 +9,14 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import org.egg.netty.file.handler.FileServerHandler;
+
+import javax.net.ssl.KeyManagerFactory;
+import java.io.InputStream;
+import java.security.KeyStore;
 
 public class FileServer {
     private static final String FILE_PATH = "/Users/adan/code/egg/netty-practice-30/src/main/resources/secret-document.txt";
@@ -18,6 +24,8 @@ public class FileServer {
     public static void main(String[] args) throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        SslContext sslContext = sslContext();
 
         try {
             ServerBootstrap bootstrap = new ServerBootstrap()
@@ -27,6 +35,8 @@ public class FileServer {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline()
+                                    .addLast(sslContext.newHandler(ch.alloc()))
+
                                     .addLast(new StringDecoder())
                                     .addLast(new ChunkedWriteHandler())
                                     .addLast(new FileServerHandler())
@@ -48,4 +58,20 @@ public class FileServer {
         }
 
     }
+
+    private static SslContext sslContext() throws Exception{
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        try (InputStream inputStream = FileServer.class.getResourceAsStream("/server.p12")){
+            keyStore.load(inputStream, "serverpass".toCharArray());
+        }
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(keyStore, "serverpass".toCharArray());
+
+        return SslContextBuilder.forServer(kmf)
+                .protocols("TLSv1.3", "TLSv1.2")
+                .build()
+                ;
+    }
+
 }
